26f1cf53510f9fc0d97c5fd1ab0e00a829bdc464
Charles Comeau <charlescomeau02@gmail.com>
2019-05-20 20:00:45 -0400

    appli fonctionnel manque des ajouts et css

--- Heroscape_appli/app.R
+++ Heroscape_appli/app.R
@@ -5,39 +5,44 @@ library(shiny)
 library(shinyWidgets)
 source("heroscape_fonction_dom.R")
 
-# Define UI for application 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Distribution des dommages"),
-
-    setBackgroundImage(src = "wp_heroscape772_1280_original.jpg"),
     
+    titlePanel("Distribution des dommages"),
     
 
     sidebarLayout(
         sidebarPanel(
-            radioButtons("nb,attaque", "Nombre d'attaque",
+            radioButtons("nb_attaque", "Nombre d'attaque",
                          choices = c("1" = 1, "2" = 2, "3" = 3, "4" = 4)),
             
             numericInput("nb_de_att", "nombre de des de l'attaquant", value = 1, min = 1, max = 15, step = 1),
             numericInput("nb_de_def", "nombre de des du defenseur", value = 1, min = 1, max = 15, step = 1),
+            numericInput("nb_vie_def", "nombre de vie du defenseur", value = 1, min = 1, max = 10, step = 1),
             checkboxInput("counter_attack", "Contre-attaque", value = FALSE)
             
         ),
-
+        
         
         mainPanel(
-            
+            verbatimTextOutput("Dommage")
         )
     )
 )
 
-# Define server 
-server <- function(input, output) {
 
+server <- function(input, output) {
+    
+    output$Dommage <- reactive({
+        ele <- Dommage(input$nb_attaque, input$nb_de_att, input$nb_de_def, input$nb_vie_def)
+        vec <- ''
+        for(i in 1:length(ele)){
+            vec <- paste(paste("probabilite de faire", (i - 1), "dommages est de :", toString(format(ele[i], digits = 6)), sep = ' '), vec, sep = '\n')
+        }
+        return(vec)
+    })
     
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
+
