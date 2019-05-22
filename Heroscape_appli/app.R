d8c72d8e3a6d3ef2d6175b3542add5706de35449
Charles Comeau <charlescomeau02@gmail.com>
2019-05-21 21:27:55 -0400

    Version debug des string

--- Heroscape_appli/app.R
+++ Heroscape_appli/app.R
@@ -7,24 +7,28 @@ source("heroscape_fonction_dom.R")
 
 ui <- fluidPage(
     
-    titlePanel("Distribution des dommages"),
+    titlePanel(title = h2("Distribution des dommages", align = "center")), br(),
     
 
     sidebarLayout(
         sidebarPanel(
-            radioButtons("nb_attaque", "Nombre d'attaque",
-                         choices = c("1" = 1, "2" = 2, "3" = 3, "4" = 4)),
-            
-            numericInput("nb_de_att", "nombre de des de l'attaquant", value = 1, min = 1, max = 15, step = 1),
-            numericInput("nb_de_def", "nombre de des du defenseur", value = 1, min = 1, max = 15, step = 1),
-            numericInput("nb_vie_def", "nombre de vie du defenseur", value = 1, min = 1, max = 10, step = 1),
+            selectInput("nb_attaque", "Nombre d'attaque", selected = 1, choices = c("1" = 1, "2" = 2, "3" = 3, "4" = 4)), br(),
+            sliderInput("nb_de_att", "nombre de des de l'attaquant", value = 1, min = 1, max = 15, step = 1), br(),
+            sliderInput("nb_de_def", "nombre de des du defenseur", value = 1, min = 1, max = 15, step = 1), br(),
+            sliderInput("nb_vie_def", "nombre de vie du defenseur", value = 1, min = 1, max = 10, step = 1), br(),
             checkboxInput("counter_attack", "Contre-attaque", value = FALSE)
             
         ),
         
         
         mainPanel(
-            verbatimTextOutput("Dommage")
+            
+            tabsetPanel(type = "tab", 
+                        tabPanel('Sommaire', verbatimTextOutput("distr_dommages"), verbatimTextOutput("attaque_nec")),
+                        tabPanel('Graphiques')
+                        
+                        )
+            
         )
     )
 )
@@ -32,17 +36,36 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     
-    output$Dommage <- reactive({
-        ele <- Dommage(input$nb_attaque, input$nb_de_att, input$nb_de_def, input$nb_vie_def)
-        vec <- ''
-        for(i in 1:length(ele)){
-            vec <- paste(paste("probabilite de faire", (i - 1), "dommages est de :", toString(format(ele[i], digits = 6)), sep = ' '), vec, sep = '\n')
+    output$distr_dommages <- renderText({
+        
+        ele_1 <- Dommage(input$nb_attaque, input$nb_de_att, input$nb_de_def, input$nb_vie_def)
+        message_1 <- "probabilite de faire"
+        text_1 <- ''
+        
+        for(i in 1:length(ele_1)){
+            text_1 <- paste(paste(message_1, (i - 1), "dommages est de :", toString(format(ele_1[i], digits = 6)), sep = ' '), text_1, sep = '\n')
         }
-        return(vec)
+        
+        return(text_1)
+    })
+    
+    output$attaque_nec <- renderText({
+        
+        message_2 <- "probabilite que le defenseur soit encore vivant apres"
+        text_2 <- ''
+        prob <- numeric(input$nb_attaque)
+        
+        for(j in 1:(input$nb_attaque)){
+            prob[j] <-  1 - Dommage(j, input$nb_de_att, input$nb_de_def, input$nb_vie_def)[input$nb_vie_def + 1]
+            text_2 <- paste(paste(message_2, j, "attaque(s) : ", toString(format(prob[j], digits = 6)), sep = ' '), text_2, sep = '\n')
+        }        
+
+        return(text_2)
     })
     
 }
 
 
 shinyApp(ui = ui, server = server)
+
 
